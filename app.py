d4e86ce42ed14178a2deabc1f01a1af0b8f2ad77
Tabish Shaukat <tabishaukat@gmail.com>
2022-12-10 13:33:35 +0530

    files req for deployement

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pickle
+import numpy as np
+#import sklearn as sk
+
+# import the model
+#reg_rf = pickle.load(open('reg_rf.pkl','rb'))
+#scalar = pickle.load(open('scaling.pkl','rb'))
+
+
+html_temp = """
+<div style="background-color:tomato;padding:10px">
+<h2 style="color:white;text-align:center;">ML App for EDM </h2>
+<h3 style="color:white;text-align:center;">(EN31 STEEL)</h3>
+</div>
+"""
+st.markdown(html_temp,unsafe_allow_html=True)
+st.title("Material Removal Rate(MRR) & Surface Roughness(Ra) Predictor : ")
+
+
+# Pulse_on_time
+Pulse_on_time = st.number_input('Pulse_on_time')
+
+#Pulse_off_time
+Pulse_off_time = st.number_input('Pulse_off_time')
+
+#Discharge_current
+Discharge_current = st.number_input('Discharge_current')
+
+# Voltage
+Voltage = st.selectbox('Voltage(V)',[10,20,30,40,50,60,70,80,90,100,110,120])
+
+
+if st.button('Predict Ra'):
+    pass
+
+if st.button('Predict MRR'):
+    pass
+    # query = np.array([Pulse_on_time,Pulse_off_time,Discharge_current,Voltage])
+    # final_query = scalar.transform(query.reshape(1,4))
+    # ## query = query.reshape(1,4)
+    # st.title("The predicted Value of MRR is " + str(reg_rf.predict(final_query)[0]))
+
+    
